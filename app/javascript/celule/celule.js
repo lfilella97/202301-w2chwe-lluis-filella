cf4110328577bedd6f500c93c4d3ba0123c121bf
lfilella97 <lfilella97@gmail.com>
2023-01-21 15:56:50 +0100

    add && to ask if the relative cel exists to avoid error

--- app/javascript/celule/celule.js
+++ app/javascript/celule/celule.js
@@ -4,19 +4,21 @@ class Celule {
   positionY;
 
   constructor(positionX, positionY) {
-    this.isAlive = Math.floor(Math.random() * 2);
+    this.isAlive = Boolean(Math.floor(Math.random() * 2));
     this.positionX = positionX;
     this.positionY = positionY;
   }
 
   runSurvivalCheck(table) {
     let survivorsArroud = 0;
-    if (this.isAlive) {
-      for (let i = -1; i <= 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-          if (table[this.positionX - i][this.positionY - j].isAlive) {
-            survivorsArroud += 1;
-          }
+
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        if (
+          table[this.positionX + i][this.positionY + j] &&
+          table[this.positionX + i][this.positionY + j].isAlive
+        ) {
+          survivorsArroud++;
         }
       }
     }

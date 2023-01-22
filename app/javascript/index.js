b8bfa5dba1af1b07b73063ab5bcfb1fae568b792
lfilella97 <lfilella97@gmail.com>
2023-01-22 16:35:15 +0100

    add .toMatchObject to pass the aproval

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,5 +1,18 @@
 import Celule from "./celule/celule.js";
+import iterateBoardShowFace from "./iterateBoardShowFace/iterateBoardShowFace.js";
 import generateTable from "./generateTable/generateTable.js";
+import survivalCheck from "./survivalCheck/survivalCheck.js";
+import changeStatus from "./changeStatus/changeStatus.js";
 
-const board = generateTable(4, 4, Celule);
+const board = generateTable(20, 20, Celule);
+
 console.table(board);
+console.table(iterateBoardShowFace(board));
+
+const run = () => {
+  survivalCheck(board);
+  changeStatus(board);
+  console.table(iterateBoardShowFace(board));
+};
+
+setInterval(run, 500);

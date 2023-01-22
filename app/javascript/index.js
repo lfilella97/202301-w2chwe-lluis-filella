ffbd53acfbead83b263f3340841af11a5c6756a8
lfilella97 <lfilella97@gmail.com>
2023-01-22 20:26:02 +0100

    add table to html to show game

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,18 +1,25 @@
 import Celule from "./celule/celule.js";
-import iterateBoardShowFace from "./iterateBoardShowFace/iterateBoardShowFace.js";
+
 import generateTable from "./generateTable/generateTable.js";
 import survivalCheck from "./survivalCheck/survivalCheck.js";
 import changeStatus from "./changeStatus/changeStatus.js";
 
-const board = generateTable(20, 20, Celule);
-
-console.table(board);
-console.table(iterateBoardShowFace(board));
+const board = generateTable(26, 57, Celule);
 
 const run = () => {
   survivalCheck(board);
   changeStatus(board);
-  console.table(iterateBoardShowFace(board));
+  const HTMLtable = document.querySelector("table");
+  HTMLtable.innerHTML = null;
+  for (let i = 0; i < board.length; i++) {
+    const row = document.createElement("tr");
+    HTMLtable.appendChild(row);
+    for (let j = 0; j < board[i].length; j++) {
+      const cell = document.createElement("td");
+      row.appendChild(cell);
+      cell.innerHTML = board[i][j].face;
+    }
+  }
 };
 
 setInterval(run, 500);

df66fca6b41bc2c6138338e330c34461035685de
lfilella97 <lfilella97@gmail.com>
2023-01-22 01:15:14 +0100

    add function that generates table with celules

---
+++ app/javascript/generateTable/generateTable.test.js
@@ -0,0 +1,32 @@
+import generateTable from "./generateTable";
+
+describe("Given the function generateTable", () => {
+  describe("When it receives 5 columns 4 rows and Array Class", () => {
+    test("Then it should return a an array, with four arrays inside that have five arrays.", () => {
+      const givenRows = 2;
+      const givenColumns = 2;
+      const givenClass = Array;
+      const expectedOutput = [
+        [
+          [0, 0],
+          [0, 1],
+          [0, 2],
+        ],
+        [
+          [1, 0],
+          [1, 1],
+          [1, 2],
+        ],
+        [
+          [2, 0],
+          [2, 1],
+          [2, 2],
+        ],
+      ];
+
+      const result = generateTable(givenRows, givenColumns, givenClass);
+
+      expect(result).toBe(expectedOutput);
+    });
+  });
+});
--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,5 +1,5 @@
 import Celule from "./celule/celule.js";
+import generateTable from "./generateTable/generateTable.js";
 
-const celule = new Celule();
-
-console.table(celule);
+const board = generateTable(4, 4, Celule);
+console.table(board);

17f3319ac612b037cd0497af9517dcab7979ab14
lfilella97 <lfilella97@gmail.com>
2023-01-23 00:18:53 +0100

    delete .js file

--- app/javascript/htmlTable/htmlTable.js
+++ app/javascript/htmlTable/htmlTable.js
@@ -2,10 +2,10 @@ const htmlTable = (board) => {
   const HTMLtable = document.querySelector("table");
   HTMLtable.innerHTML = null;
   for (let i = 0; i < board.length; i++) {
-    const row = document.createElement("td");
+    const row = document.createElement("tr");
     HTMLtable.appendChild(row);
     for (let j = 0; j < board[i].length; j++) {
-      const cell = document.createElement("th");
+      const cell = document.createElement("td");
       row.appendChild(cell);
       cell.innerHTML = board[i][j].face;
     }
